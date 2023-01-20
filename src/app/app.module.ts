efc76820f024185e36051d6f63155b72030bf374
manoj-fleapo <106957653+manoj-fleapo@users.noreply.github.com>
2023-01-20 10:56:05 +0530

    property binding complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from 'src/container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
-  declarations: [AppComponent, ContainerComponent],
+  declarations: [AppComponent, ContainerComponent, NavbarComponent, DisplayComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/display/display.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.css'],
+})
+export class DisplayComponent {
+  slogan: string = 'hello this is manoj santra we develop fantastic web app';
+  imgSrc: string = '../../assets/myimg.jpg';
+  showMyName: boolean = false;
+  name: string = 'Hi myself Manoj Santra';
+  getSlogan() {
+    return 'this is a new slogan for this web app';
+  }
+}
