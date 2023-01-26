3818f8ae1adf2b508f2cd2428a4b9843954c1b4e
Manoj Santra <santramanoj1997@gmail.com>
2023-01-26 23:20:11 +0530

    rxjs practicing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 // import { CustomService } from './services/myservice.service';
+import { Observable, from, of } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   // providers:[CustomService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-angular';
   public name = 'manoj santra';
+  // myObservable = new Observable((observer) => {
+  //   console.log('observable start : ');
+  //   observer.next('hello');
+  //   observer.next('manoj');
+  //   observer.next("what's");
+  //   observer.next('up');
+  // });
+  array1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+  array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  myObservable = from(this.array2);
+  newObservable = this.myObservable.pipe(map((val) => val * 5));
+  newObservable2 = this.newObservable.pipe(filter((val) => val % 2 === 0));
+  ngOnInit() {
+    this.newObservable2.subscribe((value) => {
+      console.log('observer val : ', value);
+    });
+  }
 }
